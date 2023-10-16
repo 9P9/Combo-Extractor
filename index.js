184d99e603e9e9ff1aad4a33b4ceb612893156ae
xss <59532064+9P9@users.noreply.github.com>
2023-10-16 17:48:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,37 @@
-const fs = require(`fs`); 
-const chalk = require("chalk");
-const readline = require("readline");
+const fs = require('fs').promises;
 
-var done = 0;
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+let done = 0;
 
-function write(content, file) {
-    fs.appendFile(file, content, function(err) {});
+async function write(content, file) {
+    await fs.appendFile(file, content);
 }
-function clear(file) {
-    var stream = fs.createWriteStream(file);
-    stream.once('open', function(fd) {
-        stream.write("");
-        stream.end();
-    });
+
+async function clear(file) {
+    await fs.truncate(file, 0);
 }
-clear("parsed.txt");
-process.title = `[313] Combo Extractor`;
-console.log(`
- ██████╗ ██████╗ ███╗   ███╗██████╗  ██████╗     ███████╗██╗  ██╗████████╗██████╗  █████╗  ██████╗████████╗ ██████╗ ██████╗ 
-██╔════╝██╔═══██╗████╗ ████║██╔══██╗██╔═══██╗    ██╔════╝╚██╗██╔╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗
-██║     ██║   ██║██╔████╔██║██████╔╝██║   ██║    █████╗   ╚███╔╝    ██║   ██████╔╝███████║██║        ██║   ██║   ██║██████╔╝
-██║     ██║   ██║██║╚██╔╝██║██╔══██╗██║   ██║    ██╔══╝   ██╔██╗    ██║   ██╔══██╗██╔══██║██║        ██║   ██║   ██║██╔══██╗
-╚██████╗╚██████╔╝██║ ╚═╝ ██║██████╔╝╚██████╔╝    ███████╗██╔╝ ██╗   ██║   ██║  ██║██║  ██║╚██████╗   ██║   ╚██████╔╝██║  ██║
- ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═════╝  ╚═════╝     ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
-`);
 
+async function extract(splitter, pos) {
+  try {
+    const combos = (await fs.readFile('combos.txt', 'utf-8')).replace(/\r/gi, '').split('\n');
+    process.title = `[313] Combo Extractor | Size: ${combos.length}`;
 
-rl.question(chalk.hex("DF362D")("[?] What symbol are you trying to split from? "), async (option) => {
-	let splitter = option;
-	console.log(`[!] Symbol Set to ${splitter}`);
-	rl.question(chalk.hex("DF362D")(`[?] What part of the String are you trying to obtain e.g hello:world - "Hello" would = 1? `), async (option) => {
-		let pos = option;
-		console.log(`[!] Position: ${pos}`);
-		extract(splitter, pos);
-	})
-})
-
-function extract(splitter, pos) {
-	const combos = fs.readFileSync('combo.txt', 'utf-8').replace(/\r/gi, '').split('\n');
-	process.title = `[313] Combo Extractor | Size: ${combos.length}`;
-    var i = 0;
-    var int = setInterval(() => {
-        if (i >= combos.length) return clearInterval(int);
-        try{
-			var combo = combos[i++];
-			var a = combo.split(splitter)[pos - 1];
-			done++;
-		    write(`${a}\n`,`parsed.txt`);		
-			process.title = `[313] Combo Extractor | Size: ${combos.length} | ${done}/${combos.length}`;
-		}
-		catch(err){
-
-		}
-    });
-	
+    for (let i = 0; i < combos.length; i++) {
+      const combo = combos[i];
+      try {
+        const a = combo.split(splitter)[pos - 1];
+        done++;
+        await write(`${a}\n`, 'parsed.txt');
+        process.title = `[313] Combo Extractor | Size: ${combos.length} | ${done}/${combos.length}`;
+      } catch (err) {
+      }
+    }
+  } catch (err) {
+    console.error(`Error reading combos.txt: ${err}`);
+  }
 }
+async function main() {
+  await clear('parsed.txt');
+  await extract(':', 1);
+}
+
+main();
